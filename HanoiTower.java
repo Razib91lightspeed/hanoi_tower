3b6cbc12dc5a30ff71996e2ec059ceeb09eaefec
razib.hasan <razib.hasan@tuni.fi>
2023-01-30 00:48:17 +0200

    hanoitower

---
+++ HanoiTower.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HanoiTower {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of disks: ");
+        int n = sc.nextInt();
+        hanoi(n, 'A', 'B', 'C');
+    }
+
+    public static void hanoi(int n, char from, char to, char aux) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + from + " to " + to);
+            return;
+        }
+        hanoi(n - 1, from, aux, to);
+        System.out.println("Move disk " + n + " from " + from + " to " + to);
+        hanoi(n - 1, aux, to, from);
+    }
+}
